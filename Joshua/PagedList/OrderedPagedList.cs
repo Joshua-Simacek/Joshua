d22f373c8716cee926f322627d11317c3016b23e
Joshua <joshuasimacek@gmail.com>
2021-06-09 12:01:38 -0400

    Add project files.

---
+++ Joshua/PagedList/OrderedPagedList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Joshua.PagedList
+{
+    public class OrderedPagedList<T> : PagedList<T>, IOrderedPagedList<T>
+    {
+        public string CurrentSort { get; }
+        public Order CurrentOrder { get; }
+
+        public OrderedPagedList(IQueryable<T> source, string orderBy, Order orderDirection = Order.Ascending, int? page = null, int pageSize = 10)
+            : base(source.OrderBy(orderBy, orderDirection), page, pageSize)
+        {
+            CurrentSort = orderBy;
+            CurrentOrder = orderDirection;
+        }
+
+        public OrderedPagedList(IEnumerable<T> source, string orderBy, Order orderDirection = Order.Ascending, int? page = null, int pageSize = 10)
+            : this(source.AsQueryable<T>(), orderBy, orderDirection, page, pageSize) { }
+    }
+}
---
+++ Joshua/PagedList/PagedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Joshua.PagedList
+{
+    public class PagedList<T> : List<T>, IPagedList<T>
+    {
+        public int TotalCount { get; }
+        public int PageCount { get; }
+        public int PageSize { get; }
+        public int CurrentPage { get; }
+
+        /// <param name="source">original collection of data</param>
+        /// <param name="page">The current "Page" out of the total amount of pages</param>
+        /// <param name="pageSize">If Pagesize is less than 0 the entire result set will be returned</param>
+        public PagedList(IQueryable<T> source, int? page, int pageSize = 10)
+        {
+            TotalCount = source.Count();
+            PageCount = pageSize > 0 ? (int)Math.Ceiling((decimal)TotalCount / (decimal)pageSize) : (pageSize < 0 ? 1 : 0);
+            PageSize = pageSize;
+            CurrentPage = page ?? 1;
+
+            this.AddRange(pageSize < 0 ? source.ToList() : source.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList());
+        }
+
+        public PagedList(IEnumerable<T> source, int? page, int pageSize = 10) : this(source.AsQueryable<T>(), page, pageSize) { }
+
+    }
+}
